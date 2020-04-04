25e16a8350ee13b713b6a9690759e29613a77b9c
Minje <alswp625@naver.com>
2020-04-05 00:08:46 +0900

    Solve ex10171

---
+++ ex10171_고양이/ex10171.java
@@ -0,0 +1,10 @@
+package ex10171_°í¾çÀÌ;
+
+public class ex10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
