94d9591c28ae7f05ae90ecad86d36027b79912cd
Minje <alswp625@naver.com>
2020-04-09 20:16:37 +0900

    Solve ex3046

---
+++ ex3046_R2/ex3046.java
@@ -0,0 +1,14 @@
+package ex3046_R2;
+import java.util.Scanner;
+
+public class ex3046 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int R1 = sc.nextInt();
+		int S = sc.nextInt();
+		
+		int R2 = 2*S-R1;
+		System.out.println(R2);
+	}
+}
