e4e4244f56c1bf81264ef5ddec812db8ca1530d9
Minje <alswp625@naver.com>
2020-04-13 21:59:52 +0900

    Solve ex1120

---
+++ ex1120_문자열/ex1120.java
@@ -0,0 +1,32 @@
+package ex1120_¹®ÀÚ¿­;
+import java.util.*;
+import java.io.*;
+
+public class ex1120 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String [] A = st.nextToken().split("");
+		String [] B = st.nextToken().split("");
+		int count=0, minCount = A.length;
+		
+		br.close();
+		int seat = 0;
+		
+		while(seat+A.length <= B.length) {
+			count = 0;
+			for(int i=0; i<A.length; i++) {
+				if(!A[i].equals(B[i+ seat])) {
+					System.out.println("seat: " + seat);
+					count++;
+				}
+			}
+			seat++;
+			if(minCount>count) {
+				minCount = count;
+			}
+		}
+		System.out.println(minCount);
+	}
+}
