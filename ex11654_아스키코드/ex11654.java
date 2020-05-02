d152963a0101a0ba1db8464bfd7737b9cd8404a4
Minje <alswp625@naver.com>
2020-05-02 18:26:45 +0900

    Solve ex11654

---
+++ ex11654_아스키코드/ex11654.java
@@ -0,0 +1,10 @@
+package ex11654_아스키코드;
+import java.util.Scanner;
+
+public class ex11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char n = sc.next().charAt(0);
+		System.out.println((int) n);
+	}
+}
