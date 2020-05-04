a73766b2564660cc7b6803f3fa258ea02a276c60
Minje <alswp625@naver.com>
2020-05-04 16:51:02 +0900

    Solve ex2439

---
+++ ex2439_별찍기2/ex2439.java
@@ -0,0 +1,18 @@
+package ex2439_º°Âï±â2;
+import java.util.Scanner;
+
+public class ex2439 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=0; i<n; i++) {
+			for(int j=n-1; j>i; j--) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
