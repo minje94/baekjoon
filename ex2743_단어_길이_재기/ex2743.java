e91a25bfbcfee8ccef4325fab98793d8702c1f45
Minje <alswp625@naver.com>
2020-07-28 23:24:15 +0900

    Solve ex2743:

---
+++ ex2743_단어_길이_재기/ex2743.java
@@ -0,0 +1,11 @@
+package ex2743_단어_길이_재기;
+import java.util.Scanner;
+
+public class ex2743 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.next();
+		
+		System.out.println(word.length());
+	}
+}
